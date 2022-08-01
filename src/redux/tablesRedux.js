569949e84d669cb40de19051f4fe279da6bb178b
Kamil <kaz4486@gmail.com>
2022-08-01 13:05:11 +0200

    Change UPDATE_TABLES name to GET_TABLES, Create UPDATE_TABLES action

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -7,17 +7,20 @@ export const getTableById = ({ tables }, tableId) =>
 
 //action names
 const createActionName = (name) => `app/tables/${name}`;
+const GET_TABLES = createActionName('GET_TABLES');
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
 
 // action creators
+export const getTables = (payload) => ({ type: GET_TABLES, payload });
 export const updateTables = (payload) => ({ type: UPDATE_TABLES, payload });
+
 export const fetchTables = (setPending) => {
   setPending(true);
   return (dispatch) => {
     fetch(`${API_URL}/tables`)
       .then((res) => res.json())
       .then((tables) => {
-        dispatch(updateTables(tables));
+        dispatch(getTables(tables));
         setPending(false);
       });
   };
@@ -46,9 +49,14 @@ export const patchTable = (id, { ...tableData }) => {
 };
 
 const tablesReducer = (statePart = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case UPDATE_TABLES:
+    case GET_TABLES:
       return [...action.payload];
+    case UPDATE_TABLES:
+      return statePart.map((table) =>
+        table.id === action.payload.id ? { ...action.payload } : table
+      );
     default:
       return statePart;
   }
